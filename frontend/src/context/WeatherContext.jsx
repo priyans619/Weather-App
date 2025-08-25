7e844936c332fe05c79560329519226da6825aa6
priyans619 <priyansh.2b@gmail.com>
2025-08-25 18:45:02 +0530

    add: context provider for handling global state

---
+++ frontend/src/context/WeatherContext.jsx
@@ -0,0 +1,39 @@
+// src/context/WeatherContext.js
+import { createContext, useContext, useState } from "react";
+import { getCurrentWeather, getForecast } from "../api/weatherApi";
+
+const WeatherContext = createContext();
+
+export const WeatherProvider = ({ children }) => {
+  const [weather, setWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchWeatherData = async (city) => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const current = await getCurrentWeather(city);
+      const forecastData = await getForecast(city);
+
+      setWeather(current);
+      setForecast(forecastData.list);
+    } catch (err) {
+      setError("Failed to fetch weather data");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <WeatherContext.Provider
+      value={{ weather, forecast, loading, error, fetchWeatherData }}
+    >
+      {children}
+    </WeatherContext.Provider>
+  );
+};
+
+export const useWeather = () => useContext(WeatherContext);
