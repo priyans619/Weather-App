d1b9c5dc9e27eaee44bde99fd0f38b76f3523ddd
priyans619 <priyansh.2b@gmail.com>
2025-08-25 13:56:56 +0530

    add: /api/weather entry route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import weatherRoutes from "./routes/weatherRoutes.js";
 
 dotenv.config();
 
@@ -9,9 +10,14 @@ const app = express();
 // Connect to DB
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+// app.get("/", (req, res) => {
+//   res.send("API is running...");
+// });
+
+app.use(express.json());
+
+// Main API route to connect to backend
+app.use("/api/weather", weatherRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
