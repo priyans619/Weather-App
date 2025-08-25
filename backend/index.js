91e75859bf23cf74122899f5bbfd66aca13b6e62
priyans619 <priyansh.2b@gmail.com>
2025-08-25 16:49:31 +0530

    index.js server update

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,14 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
 import connectDB from "./config/db.js";
 import weatherRoutes from "./routes/weatherRoutes.js";
-
-dotenv.config();
 
 const app = express();
 
 // Connect to DB
 connectDB();
-
-// app.get("/", (req, res) => {
-//   res.send("API is running...");
-// });
 
 app.use(express.json());
 
