a3e62c3c204b34d1286b4e12ede58be29b6d71e9
priyans619 <priyansh.2b@gmail.com>
2025-08-26 14:22:48 +0530

    update: Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@
 import SearchBar from "../components/SearchBar";
 import WeatherCard from "../components/WeatherCard";
 import Forecast from "../components/Forecast";
+import FavoriteCities from "../components/FavoriteCities";
 import { useWeather } from "../context/WeatherContext";
 
 const Home = () => {
@@ -9,17 +10,19 @@ const Home = () => {
 
   return (
     <div className="w-full min-h-screen bg-gradient-to-br from-sky-950 to-blue-900 text-white px-4 py-6">
-      <h1 className="text-3xl text-center text-black font-semibold mb-6">
+      <h1 className="text-3xl text-center text-black font-semibold">
         WEATHER APPLICATION
       </h1>
 
       <SearchBar />
 
-      {loading && <p className="text-center text-gray-300">Loading...</p>}
-      {error && <p className="text-center text-red-400">{error}</p>}
+      <div className="h-6 flex items-center justify-center">
+        {loading && <p className="text-gray-300">Loading...</p>}
+        {error && <p className="text-red-400">{error}</p>}
+      </div>
 
       {/* Weather & Forecast parallel layout */}
-      <div className="flex flex-col lg:flex-row justify-center items-stretch gap-6 mt-6 max-w-7xl mx-auto px-2">
+      <div className="flex flex-col lg:flex-row justify-center items-stretch gap-6 mb-10 max-w-7xl mx-auto px-2">
         <div className="w-full lg:w-1/2">
           <WeatherCard />
         </div>
@@ -27,6 +30,7 @@ const Home = () => {
           <Forecast />
         </div>
       </div>
+      <FavoriteCities />
     </div>
   );
 };
