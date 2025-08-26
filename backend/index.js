5633f7f710550c5b62b7cec6f45d3e855f58327a
priyans619 <priyansh.2b@gmail.com>
2025-08-26 14:07:11 +0530

    add cityToute to server point

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config();
-
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import weatherRoutes from "./routes/weatherRoutes.js";
+import cityRoutes from "./routes/cityRoutes.js";
 
 const app = express();
+connectDB();
 
 // setting cors for unique url
 app.use(
@@ -17,13 +18,12 @@ app.use(
   })
 );
 
-// Connect to DB
-connectDB();
 
 app.use(express.json());
 
 // Main API route to connect to backend
 app.use("/api/weather", weatherRoutes);
+app.use("/api/cities", cityRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
