3a43ca10b8723aea2808efd74362df5e9a19e042
priyans619 <priyansh.2b@gmail.com>
2025-08-26 12:43:29 +0530

    search icon

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,6 +1,7 @@
 // src/components/SearchBar.jsx
 import { useState } from "react";
 import { useWeather } from "../context/WeatherContext";
+import { FiSearch } from "react-icons/fi";
 
 const SearchBar = () => {
   const [city, setCity] = useState("");
@@ -14,20 +15,24 @@ const SearchBar = () => {
   };
 
   return (
-    <div className="flex items-center gap-2 p-4">
-      <input
-        type="text"
-        placeholder="Enter city..."
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-        className="border p-2 rounded w-full"
-      />
-      <button
-        onClick={handleSearch}
-        className="bg-green-600 text-white px-4 py-2 rounded"
-      >
-        Search
-      </button>
+    <div className="text-center px-4 md:px-8 py-4 md:py-8">
+      <div className="relative w-full md:w-[700px] mx-auto">
+        
+        <div className="absolute inset-y-0 left-3 flex items-center pointer-events-none text-gray-400">
+          <FiSearch size={20} />
+        </div>
+
+        <input
+          type="text"
+          placeholder="Enter Location..."
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && handleSearch()}
+          className="py-2 pl-10 pr-3 w-full text-lg rounded-3xl
+            border border-gray-200 text-gray-600 placeholder:text-gray-400
+            focus:outline-none bg-white shadow-md"
+        />
+      </div>
     </div>
   );
 };
