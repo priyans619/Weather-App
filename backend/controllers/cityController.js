d9d75793ca2eaffd5bb47b4a3f808c7307904cb1
priyans619 <priyansh.2b@gmail.com>
2025-08-26 13:52:57 +0530

    all cities+remove city fucntionality

--- backend/controllers/cityController.js
+++ backend/controllers/cityController.js
@@ -20,3 +20,22 @@ export const addCity = async (req, res) => {
   }
 };
 
+// Get all saved cities
+export const getCities = async (req, res) => {
+  try {
+    const cities = await City.find();
+    res.json(cities);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Remove a city
+export const removeCity = async (req, res) => {
+  try {
+    await City.findByIdAndDelete(req.params.id);
+    res.json({ message: "City removed" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
